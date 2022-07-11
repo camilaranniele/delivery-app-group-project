c95006a71cf553c77beae5c110878689908e37cc
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-11 19:18:02 -0300

    chore: geracao da tabela de vendas no sequelize

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sales = sequelize.define('Sales', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    sellerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL(9, 2),
+      allowNull: false
+    },
+    deliveryAddress: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deliveryNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    saleDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  });
+
+  Sales.associate = (models) => {
+    Sales.belongsTo(models.Users, {
+      foreignKey: 'userId', as: 'user',
+    });
+    Sales.belongsTo(models.Users, {
+      foreignKey: 'sellerId', as: 'seller',
+    });
+  };
+
+  return Sales;
+};
