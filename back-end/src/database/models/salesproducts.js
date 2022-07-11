7964e769331bcc3dc38fc782f923a56fed2fad48
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-11 19:19:19 -0300

    chore: geracao da tabela de vendas e produtos no sequelize

---
+++ back-end/src/database/models/salesproducts.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const SalesProducts = sequelize.define('SalesProducts', {
+    saleId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  });
+
+  SalesProducts.associate = (models) => {
+    models.Sales.belongsToMany(models.Products, {
+      as: 'products',
+      through: SalesProducts,
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+    });
+    models.Products.belongsToMany(models.Sales, {
+      as: 'sales',
+      through: SalesProducts,
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+    });
+  }
+
+  return SalesProducts;
+};
