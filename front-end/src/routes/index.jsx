3720aa841d9de4be80ef10ff6d5e3260e93d7e14
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-12 19:54:47 -0300

    feat: adicionando rotas

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import LoginForm from '../components/form/LoginForm';
+import Login from '../pages/Login';
+import LoginRedirect from '../pages/LoginRedirect';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ LoginForm } />
+        <Route exact path="/" component={ LoginRedirect } />
         <Route exact path="/login" component={ Login } />
-        <Route exact path="/register" component={ Register } />
-        <Route exact path="/customer/products" component={ Products } />
+        {/* <Route exact path="/register" component={ Register } />
+        <Route exact path="/customer/products" component={ Products } /> */}
       </Switch>
     );
   }
