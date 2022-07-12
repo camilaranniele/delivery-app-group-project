a252737330a16e5e975febfa2a8cfe5bfb3e2726
aparecidag365 <mgegoulart365@gmail.com>
2022-07-12 18:05:27 -0300

    feat: Alterações App

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const ProductRoutes = require('/.routes/products.routes');
+
+const cors = require(cors);
+
 const app = express();
 
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(ProductRoutes);
 
 module.exports = app;
