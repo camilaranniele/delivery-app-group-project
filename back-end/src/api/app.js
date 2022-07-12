5de022ef2457ab3295717f961306c70c752da461
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-12 16:44:47 -0300

    feat: adiciona cors na aplicacao

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const rootRouter = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
