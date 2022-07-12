aba735325e07568b43ebca3d59af42e2e1c5c723
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-12 16:43:40 -0300

    feat: rootRouter no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
+const rootRouter = require('./routes');
+
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(rootRouter);
 
 module.exports = app;
