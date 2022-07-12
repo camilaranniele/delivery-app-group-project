d1a6a3f895d9978ec285e45ceb9b40667608e16a
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-12 19:14:48 -0300

    feat: adicionando rota de login

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import LoginForm from '../components/form/LoginForm';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ Login } />
+        <Route exact path="/" component={ LoginForm } />
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/customer/products" component={ Products } />
