9b558f2b9951a76d8887d3ef0263bcb5eaf261f8
victor jardes <victor_maanoel@outlook.com>
2022-07-12 18:19:40 -0400

    feat(pages/products): cria pagina pra rendizar os componentes de produtos

---
+++ front-end/src/pages/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/navBar/NavBar';
+
+function Products() {
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default Products;
