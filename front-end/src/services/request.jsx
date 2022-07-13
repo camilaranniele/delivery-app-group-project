5dcc8b27f51057168950d754be9d4bc780dfd47b
Meiryland Melo <meirylandmelo@gmail.com>
2022-07-13 09:20:44 -0300

    fix: requestRegister

--- front-end/src/services/request.jsx
+++ front-end/src/services/request.jsx
@@ -7,9 +7,9 @@ export const requestLogin = async (url, body) => {
   return data;
 };
 
-export const requestRegistro = async (url, body) => {
-  const { data } = await api.post(url, body);
-  return data;
+export const requestRegister = async (url, body) => {
+  const response = await api.post(url, body);
+  return response;
 };
 
 export const requestProdutos = async (url) => {
