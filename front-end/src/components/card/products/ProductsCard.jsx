6f42872252301d7ddbcd9c10602c17628a1863d3
victor jardes <victor_maanoel@outlook.com>
2022-07-13 14:21:50 -0400

    feat(componentes/card/productsCard.jsx): renderiza os cards botoes e imagem

---
+++ front-end/src/components/card/products/ProductsCard.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react';
+import context from '../../../context';
+import ProductsButton from '../../button/products/ProductsButton';
+import './card.css';
+
+function ProductCard() {
+  const { products, totalPrice } = useContext(context);
+  const MAGIC_BUMBER_TO_FIXED = 2;
+
+  return (
+    <div className="box-container">
+      {
+        products.map(({ id, name, price/* , url_image: urlImage */ }) => (
+          <div
+            key={ id }
+            className="cards-main"
+          >
+            <p className="cards-products-price">
+              R$:
+              <span
+                data-testid={ `customer_products__element-card-price-${id}` }
+                className="cards-products-price"
+              >
+                { price }
+              </span>
+            </p>
+            <img
+              src="https://cf.shopee.com.br/file/8d99a10270672f2d6522762f2dd0ecf3"
+              width="200"
+              height="200"
+              alt={ `produto ${name}` }
+              data-testid={ `customer_products__img-card-bg-image-${id}` }
+            />
+            <p
+              data-testid={ `customer_products__element-card-title-${id}` }
+              className="cards-products-name"
+            >
+              {name}
+            </p>
+            <ProductsButton
+              id={ id }
+              price={ price }
+            />
+          </div>
+        ))
+      }
+      <footer>
+        <p>
+          Ver Carrinho: R$:
+          <span
+            data-testid="customer_products__checkout-bottom-value"
+          >
+            { totalPrice.toFixed(MAGIC_BUMBER_TO_FIXED) }
+          </span>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default ProductCard;
