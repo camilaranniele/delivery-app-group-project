991532c8c77b83e6aaa97e01e8799ce288c9b9b2
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-13 17:59:42 -0300

    fix: arrumando nome dos campos para snake_case

--- back-end/src/database/models/sales.js
+++ back-end/src/database/models/sales.js
@@ -7,34 +7,41 @@ module.exports = (sequelize, DataTypes) => {
     },
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      field: 'user_id'
     },
     sellerId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      field: 'seller_id'
     },
     totalPrice: {
       type: DataTypes.DECIMAL(9, 2),
-      allowNull: false
+      allowNull: false,
+      field: 'total_price'
     },
     deliveryAddress: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'delivery_address'
     },
     deliveryNumber: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'delivery_number'
     },
     saleDate: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: false,
+      field: 'sale_date'
     },
     status: {
       type: DataTypes.STRING,
       allowNull: false
     }
   }, {
-    timestamps: false
+    timestamps: false,
+    underscored: true
   });
 
   sales.associate = (models) => {
