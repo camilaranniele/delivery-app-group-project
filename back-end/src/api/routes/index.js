d29a8010c4e2de6761a9f325fd16c5def42e7366
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-13 16:59:49 -0300

    feat: usa rota para cadastrar usuario como administrador

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const ProductRoutes = require('./product.route');
 const UserRoutes = require('./user.routes');
+const AdminRoutes = require('./admin.routes');
 
 const rootRouter = Router();
 
 rootRouter.use('/users', UserRoutes);
 rootRouter.use('/products', ProductRoutes);
+rootRouter.use('/admin', AdminRoutes);
 
 module.exports = rootRouter; 
