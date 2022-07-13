2548dc91582caaf0d3899940242c1676417900a1
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-13 17:28:58 -0300

    feat: adiciona role no schema do user

--- back-end/src/api/middlewares/schemas/user.schema.js
+++ back-end/src/api/middlewares/schemas/user.schema.js
@@ -4,6 +4,7 @@ const userSchema = Joi.object().keys({
   name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  role: Joi.string(),
 });
 
 module.exports = {
