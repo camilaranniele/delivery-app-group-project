ef34d4dbdab83bd9b0d0f3f364ec4f1704138b10
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-13 16:58:33 -0300

    feat: funcao para criar usuario como administrador

--- back-end/src/api/services/user.service.js
+++ back-end/src/api/services/user.service.js
@@ -2,7 +2,7 @@ const { users } = require('../../database/models');
 
 const { hash, verify } = require('../utils/md5');
 
-const { sign } = require('../utils/jwt');
+const { sign, decode } = require('../utils/jwt');
 
 const createUser = async ({ name, email, password }) => {
   const hashedPassword = hash(password);
@@ -43,7 +43,32 @@ const login = async (email, password) => {
   };
 };
 
+const createUserByAdmin = async ({ name, email, password, role }, token) => {
+  const decodedToken = decode(token);
+
+  if (decodedToken.role !== 'administrator') {
+    return { code: 401, error: 'You are not administrator' };
+  }
+
+  const hashedPassword = hash(password);
+
+  const findUser = await users.findOne({ where: { email } });
+
+  if (findUser) {
+    return { code: 409, error: 'User already exists' };
+  }
+
+  const newUser = await users.create({ name, email, password: hashedPassword, role });
+
+  return {
+    name: newUser.name,
+    email: newUser.email,
+    role: newUser.role,
+  };
+};
+
 module.exports = {
   createUser,
   login,
+  createUserByAdmin,
 };
