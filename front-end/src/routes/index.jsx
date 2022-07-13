3e3be6f7771fc68b39c4abc3dbe476aa7ab7ccc3
victor jardes <victor_maanoel@outlook.com>
2022-07-13 14:25:12 -0400

    refactor(src/routes): comenta as rotas nao criadas

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Products from '../pages/Products/Products';
+import Products from '../pages/Products';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ Login } />
+        {/* <Route exact path="/" component={ Login } />
         <Route exact path="/login" component={ Login } />
-        <Route exact path="/register" component={ Register } />
+        <Route exact path="/register" component={ Register } /> */}
         <Route exact path="/customer/products" component={ Products } />
       </Switch>
     );
