aaa087b4d56c6682c1b0c27c7076a9d00642ebc2
Meiryland Melo <meirylandmelo@gmail.com>
2022-07-13 09:19:42 -0300

    feat: Cria página de resgistro de usuário

---
+++ front-end/src/pages/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormRegister from '../components/form/form.register';
+
+function Register() {
+  return (
+    <FormRegister />
+  );
+}
+
+export default Register;
