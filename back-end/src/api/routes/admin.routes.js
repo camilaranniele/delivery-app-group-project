034572eeb1edec4d6049cd8dd4d4c3283c6673b7
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-13 17:30:10 -0300

    feat: adiciona middlewares de validacao para criar novo usuario como administrador

--- back-end/src/api/routes/admin.routes.js
+++ back-end/src/api/routes/admin.routes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
 
 const UserController = require('../controllers/user.controller');
+const UserMiddleware = require('../middlewares/user.middleware');
+const TokenMiddleware = require('../middlewares/token.middleware');
+const { userSchema } = require('../middlewares/schemas/user.schema');
 
-const userRouter = Router();
+const adminRouter = Router();
 
-userRouter.post('/register', UserController.createUserByAdmin);
+adminRouter.post('/register',
+  (req, res, next) => TokenMiddleware.validateToken(req, res, next),
+  (req, res, next) => UserMiddleware.userValidation(req, res, next, userSchema),
+  UserController.createUserByAdmin);
 
-module.exports = userRouter;
+module.exports = adminRouter;
