20e01be6d6d7e72033a323c1872cac88fb7ce552
victor jardes <victor_maanoel@outlook.com>
2022-07-13 14:24:40 -0400

    build(rsc/provider): adicona a minima logica para criar os produtos

--- front-end/src/provider/index.jsx
+++ front-end/src/provider/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from '../context';
+// import { requestProdutos } from '../services/request';
+import productsMock from './productsMock.json';
 
 function Provider({ children }) {
-  const contextValue = {
+  const [products, setProducts] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
+  useEffect(() => {
+    async function fetchProducts() {
+      await setProducts(productsMock);
+    }
+    fetchProducts();
+  }, []);
+
+  const contextValue = {
+    products,
+    totalPrice,
+    setTotalPrice,
   };
 
   return (
