d998b81a28d5a29041c184b2fc0dd8086ea4cdb4
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-14 19:48:19 -0300

    feat: criando controller para ler todos os detalhes de uma venda

--- back-end/src/api/controllers/sales.controller.js
+++ back-end/src/api/controllers/sales.controller.js
@@ -15,6 +15,24 @@ const readSales = async (req, res) => {
   }
 };
 
+const readSalesDetails = async (req, res) => {
+  try {
+    const { id: userId, role } = req.user;
+    const { id: saleId } = req.params;
+  
+    if (role === 'administrator') {
+      return res.status(401).json({ error: '"administrator" don\'t have sales' });
+    }
+  
+    const sales = await UserService.readSalesDetails(saleId, userId, role === 'seller');
+    res.status(200).json(sales);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
   readSales,
+  readSalesDetails,
 };
