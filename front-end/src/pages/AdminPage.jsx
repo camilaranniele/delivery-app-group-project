da0fea5611b46a8e5ea4e7a205749682306a94c9
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-14 09:19:36 -0300

    feat: pagina do admin

---
+++ front-end/src/pages/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminRegister from '../components/form/Admin.register';
+
+function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <AdminRegister />
+    </div>
+  );
+}
+
+export default AdminPage;
