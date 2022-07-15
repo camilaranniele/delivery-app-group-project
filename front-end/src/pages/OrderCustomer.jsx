def1e0f46666a7c687c592a58100b6db5cc2f871
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-14 21:09:58 -0300

    feat: adicionando pagina de pedidos do cliente

---
+++ front-end/src/pages/OrderCustomer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import OrderForms from '../components/form/ViewerOrderForm';
+
+function OrderCustomer() {
+  return (
+    <OrderForms />
+  );
+}
+
+export default OrderCustomer;
