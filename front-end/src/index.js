7e0a9ad4eed0d13845045f693271a69810b27aa9
Meiryland Melo <meirylandmelo@gmail.com>
2022-07-15 18:08:27 -0300

    chore: retira objeto com theme do index

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-
-const colors = {
-  green: {
-    100: '#B1C2BE',
-    300: '#2FC18C',
-    500: '#036B52',
-    600: '#001813'
-  },
-  blue: {
-    500: '#056CF9'
-  },
-  purple: {
-    500: '#421981'
-  },
-  gray: {
-    100: '#E5E5E5'
-  }
-}
-
-const theme = extendTheme({ colors })
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -34,6 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-
-
