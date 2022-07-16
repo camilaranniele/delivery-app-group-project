f91209d8929cf168656eae41e1648cc07d27c205
victor jardes <victor_maanoel@outlook.com>
2022-07-15 22:31:22 -0400

    build(components/button/checkout): add data-testid

--- front-end/src/components/button/checkout/ButtonCheckout.jsx
+++ front-end/src/components/button/checkout/ButtonCheckout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ButtonCheckout({ removeItenInListProducts, id }) {
+function ButtonCheckout({ removeItenInListProducts, id, index }) {
   return (
     <td>
       <button
         type="button"
+        data-testid={ `customer_checkout__element-order-table-remove-${index}` }
         value={ id }
         onClick={ (e) => removeItenInListProducts(e.target) }
       >
@@ -18,6 +19,7 @@ function ButtonCheckout({ removeItenInListProducts, id }) {
 ButtonCheckout.propTypes = {
   removeItenInListProducts: PropTypes.func.isRequired,
   id: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default ButtonCheckout;
