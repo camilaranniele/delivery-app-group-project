6fff12944e9192e5838c4b7e3d5dfe8a4979bc0a
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-19 18:03:40 -0300

    feat: adiciona lógica-1 do botão

--- front-end/src/components/button/status/OrderStatusButton.jsx
+++ front-end/src/components/button/status/OrderStatusButton.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Flex } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import { changeOrderStatus } from '../../../services/request';
 
-function OrderStatusButton() {
+function OrderStatusButton({ statusDaVenda }) {
+  const { id } = useParams();
   const { role, token } = JSON.parse(localStorage.getItem('user'));
+  const emTransito = 'Em Trânsito';
+  const preparando = 'Preparando';
+  const entregue = 'Entregue';
+  const pendente = 'Pendente';
+  const [disableBtt, setDisableBtt] = useState({
+    preparing: statusDaVenda === pendente,
+    dispatch: statusDaVenda === preparando,
+    delivery: statusDaVenda === emTransito,
+  });
 
-  // useEffect(() => {
-  //   const requestApi = async () => {
-  //     const request = await requestOrderDetails(
-  //       `/sales/details/${id}`, token,
-  //     );
-  //     setOrders(request);
-  //   };
-  //   requestApi();
-  // }, [role, token]);
+  console.log(disableBtt);
 
   const handleClick = async (status) => {
-    await requestOrderDetails(
+    await changeOrderStatus(
       `/sales/status/${id}`, { status }, token,
     );
+    setDisableBtt({
+      ...disableBtt,
+      [status]: true,
+    });
   };
 
   return (
@@ -26,22 +35,28 @@ function OrderStatusButton() {
         role === 'customer'
           ? (
             <Button
+              name="delivery"
               data-testid="customer_order_details__button-delivery-check"
-              onClick={ () => handleClick('Entregue') }
+              onClick={ () => handleClick(entregue) }
+              disabled={ disableBtt.delivery }
             >
               Marcar como entregue
             </Button>
           ) : (
             <>
               <Button
+                name="preparing"
                 data-testid="seller_order_details__button-preparing-check"
-                onClick={ () => handleClick('Preparando') }
+                onClick={ () => handleClick(preparando) }
+                disabled={ disableBtt.preparing }
               >
                 Preparar pedido
               </Button>
               <Button
+                name="dispatch"
                 data-testid="seller_order_details__button-dispatch-check"
-                onClick={ () => handleClick('Em Trânsito') }
+                onClick={ () => handleClick(emTransito) }
+                disabled={ disableBtt.dispatch }
               >
                 Saiu para entrega
               </Button>
@@ -51,5 +66,9 @@ function OrderStatusButton() {
     </Flex>
   );
 }
+
+OrderStatusButton.propTypes = {
+  statusDaVenda: PropTypes.string.isRequired,
+};
 
 export default OrderStatusButton;
