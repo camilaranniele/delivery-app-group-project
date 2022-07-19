ec488c048f5227a477e425de0413e9bde04bac89
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-19 15:19:27 -0300

    feat: criando rota para mudar status do pedido

--- back-end/src/api/routes/sale.routes.js
+++ back-end/src/api/routes/sale.routes.js
@@ -3,14 +3,17 @@ const SalesController = require('../controllers/sales.controller');
 const tokenMiddleware = require('../middlewares/token.middleware');
 const schemaValidator = require('../middlewares/schemaValidator.middleware');
 const { saleSchema } = require('../middlewares/schemas/sale.schema');
+const { saleStatusSchema } = require('../middlewares/schemas/saleStatus.schema');
 
 const SalesRoutes = Router();
 const validateNewSale = schemaValidator(saleSchema);
+const validateSaleStatus = schemaValidator(saleStatusSchema);
 
 SalesRoutes.use(tokenMiddleware.validateToken);
 
 SalesRoutes.get('/', SalesController.readSales);
 SalesRoutes.get('/details/:id', SalesController.readSalesDetails);
 SalesRoutes.post('/', validateNewSale, SalesController.create);
+SalesRoutes.patch('/status/:id', validateSaleStatus, SalesController.updateStatus);
 
 module.exports = SalesRoutes;
