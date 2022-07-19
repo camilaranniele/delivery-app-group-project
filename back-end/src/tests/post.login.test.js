4918dfefa0694ff4ea921f00993fbcfee3b9dbf5
aparecidag365 <mgegoulart365@gmail.com>
2022-07-19 17:43:27 -0300

    test: Alterações teste rota login

--- back-end/src/tests/post.login.test.js
+++ back-end/src/tests/post.login.test.js
@@ -9,8 +9,6 @@ const {
   loginUser,
   createUser,
   returnRegister,
-  // invalidEmail,
-  // invalidPassword,
   returnToken,
 } = require('../../src/tests/mocks/user.mock');
 
@@ -21,7 +19,7 @@ let chaiRequest = chai.request;
 
 const { expect } = chai;
 
-describe('Testa Resposta correta para a Rota /login', () => {
+describe('Testa Resposta correta para a Rota users/login', () => {
   let OkResponse;
 
   before(async () => {
@@ -53,43 +51,4 @@ describe('Testa Resposta correta para a Rota /login', () => {
     expect(token).to.be.equal(returnRegister.token);
    
   });
-
 });
-
-  // describe('Testa validação dos campos para Rota /login', () => {
-  //   let invalidEmailRes;
-  //   // let invalidPasswordRes;
-  //   const response = {}; 
-  //   before(async () => {
-  //     sinon
-  //       .stub(users, 'findOne')
-  //       .returns(response);
-  //     invalidEmailRes = await chaiRequest(app)
-  //       .post('users/login')
-  //       .send(invalidEmail);
-      // invalidPasswordRes = await chaiRequest(app)
-      //   .post('users/login')
-      //   .send(invalidPassword);
-  
-  //  });
-  //   after(() => {
-  //     users.findOne.restore();
-  //   })
-  
-  //   it('Verifica login de um usuário sem um "email" válido', () => {
-  //     const { body } = invalidEmailRes;
-  //     console.log("BODY ", body);
-  
-  //     expect(invalidEmailRes).to.be.an('object');
-  //     expect(invalidEmailRes).to.have.status(400);
-  //     expect(body.error).to.be.equal('"email" or "password" is incorrect');
-  //   });
-  
-    // it('Verifica login de um usuário sem um "password" válido', () => {
-    //   const { body } = invalidPasswordRes;
-  
-    //   expect(invalidEmailRes).to.be.an('object');
-    //   expect(invalidEmailRes).to.have.status(400);
-    //   expect(body.error).to.be.equal('"email" or "password" is incorrect');
-    // });
-  // });
