c076cba1fff0ed5b0381276cf90468ee5a172720
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-19 18:02:29 -0300

    refactor: desvincular localhost da página de details

--- front-end/src/pages/Details.jsx
+++ front-end/src/pages/Details.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Flex } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
 import NavBar from '../components/navBar/NavBar';
-import ProductDetailTable from '../components/table/productDetail/TableDetails';
+import DetailTable from '../components/table/productDetail/TableDetails';
 import OrderDetailContainer from '../components/box/orderDetails/OrderDetailContainer';
+import { requestOrderDetails } from '../services/request';
 
 function Details() {
-  const userLocal = JSON.parse(localStorage.getItem('user'));
-  const { role } = userLocal;
+  const { token } = JSON.parse(localStorage.getItem('user'));
+  const [order, setOrder] = useState({
+    totalPrice: '',
+    products: [],
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    const requestApi = async () => {
+      const request = await requestOrderDetails(
+        `/sales/details/${id}`, token,
+      );
+      console.log(request);
+      setOrder(request);
+    };
+    requestApi();
+  }, [id, token]);
+
   return (
     <div>
       <header>
@@ -17,8 +35,8 @@ function Details() {
         flexDir="column"
       >
         <OrderDetailContainer />
-        <ProductDetailTable
-          role={ role }
+        <DetailTable
+          carrinho={ order }
         />
       </Flex>
     </div>
