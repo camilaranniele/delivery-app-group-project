6baeefe88f84efcdb11b8dba3ad376a0e26c7066
Ismael_Luviseti <ismaelluviseti@gmail.com>
2022-07-18 21:35:36 -0300

    feat: renderizando a pagina de detalhes do pedido

--- front-end/src/pages/Details.jsx
+++ front-end/src/pages/Details.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
 import NavBar from '../components/navBar/NavBar';
+import ProductDetailTable from '../components/table/productDetail/TableDetails';
+import OrderDetailContainer from '../components/box/orderDetails/OrderDetailContainer';
 
 function Details() {
+  const userLocal = JSON.parse(localStorage.getItem('user'));
+  const { role } = userLocal;
   return (
     <div>
       <header>
         <NavBar />
       </header>
+      <Flex
+        as="main"
+        flexDir="column"
+      >
+        <OrderDetailContainer />
+        <ProductDetailTable
+          role={ role }
+        />
+      </Flex>
     </div>
   );
 }
