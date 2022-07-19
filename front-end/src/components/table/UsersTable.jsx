5e43295e8b429466ed3b72563646b26793dd63ee
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-19 10:37:44 -0300

    feat: tabela que lista usuarios

---
+++ front-end/src/components/table/UsersTable.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { requestUsers } from '../../services/request';
+
+function UsersTable() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchUsers() {
+      const requestAllUsers = await requestUsers('/users');
+      setUsers(requestAllUsers);
+    }
+    fetchUsers();
+  }, [users]);
+
+  return (
+    <div>
+      <h1>Lista de usúarios</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Tipo</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(({ id, name, email, role }) => (
+              <tr key={ id }>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{email}</td>
+                <td>{role}</td>
+                <td><button type="submit">Excluir</button></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UsersTable;
