7e6f91f5ae36dc4d1058a6edf12026a7bdd9e6e9
Meiryland Melo <meirylandmelo@gmail.com>
2022-07-22 15:02:11 -0300

    style: adiciona componentes de estilizacão a page Products

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { Box, Center } from '@chakra-ui/react';
 import NavBar from '../components/navBar/NavBar';
 import ProductCard from '../components/card/products/ProductsCard';
 
 function Products() {
   return (
-    <div>
-      <header>
-        <NavBar />
-      </header>
-      <main>
+    <Box>
+      <NavBar />
+      <Center>
         <ProductCard />
-      </main>
-    </div>
+      </Center>
+    </Box>
   );
 }
 
